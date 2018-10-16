90714505eb18dc8af9effa021ad13b5118cdc698
Steven Blom <atomicblom@binaryvibrance.net>
2018-10-17 00:44:26 +1030

    More stuffs

--- DeployProject/DeployProjectV5/CurseForgeAPI/ConnectedService.ts
+++ DeployProject/DeployProjectV5/CurseForgeAPI/ConnectedService.ts
@@ -1,23 +1,29 @@
 import * as tl from 'azure-pipelines-task-lib/task';
-import { ServiceClient, ServiceClientOptions, ServiceClientCredentials } from 'ms-rest-js';
+import { ServiceClient, ServiceClientOptions, ServiceClientCredentials, TokenCredentials, ApiKeyCredentials } from 'ms-rest-js';
 
 function getCredentials(id: string, credentialsOptional: boolean) : ServiceClientCredentials | null 
 {
-    const endpointUrl = tl.getEndpointUrl(id, credentialsOptional);
-    tl.debug(`endpointUrl: ${endpointUrl}`);
-    const authorizationScheme = tl.getEndpointAuthorizationScheme(id, credentialsOptional);
-    tl.debug(`authorizationScheme: ${authorizationScheme}`);
     const authorization = tl.getEndpointAuthorization(id, credentialsOptional);
-    tl.debug(`authorization: ${authorization}`);
-    
-    return null;
+    if (authorization.scheme === "Token") {
+        tl.debug("Authorizing using API Key credentials");
+        const result = new ApiKeyCredentials({
+            inHeader: {
+                "X-Api-Token": authorization.parameters.Token
+            }
+        });
+        return result;
+    }
+    throw Error(`Unsupported authorization: ${JSON.stringify(authorization)}`);
 }
 
 export class ConnectedService extends ServiceClient {
+    private endpointUrl: string;
+
     constructor(private connectedServiceName: string, 
                 private credentialsOptional: boolean = true, 
                 private options?: ServiceClientOptions) 
     {
         super(getCredentials(connectedServiceName, credentialsOptional), options);
+        this.endpointUrl = tl.getEndpointUrl(connectedServiceName, credentialsOptional);
     }
 }
--- DeployProject/DeployProjectV5/DeployProject.ts
+++ DeployProject/DeployProjectV5/DeployProject.ts
@@ -1,15 +1,39 @@
 import tl = require('azure-pipelines-task-lib/task');
+import fs = require('fs');
 import { ConnectedService } from './CurseForgeAPI/ConnectedService';
+import { WebResource } from 'ms-rest-js';
+import { IProjectUpload } from './CurseForgeAPI/IProjectUpload';
 
 //npm install vsts-task-lib
 
 // Get task parameters
 let serviceName: string = tl.getInput('CurseForgeService', true);
-
+let file = tl.getPathInput("ProjectFile", true, true);
 async function run() {
     try {
 
-        const service = new ConnectedService(serviceName);
+        /*const service = new ConnectedService(serviceName);
+        const webResource = new WebResource();
+        
+        const requestForm = new FormData();
+        const metadata: IProjectUpload = {
+            changelog: "",
+            releaseType: "alpha",
+            gameVersions: [ 6756 ],
+        }
+
+        const reader = new FileReader();
+        reader.
+        requestForm.append("metadata", JSON.stringify(metadata));
+        requestForm.append("file", new File(); )
+
+        service.sendRequest()*/
+
+        //Almost working CURL request?:
+        //curl -X POST --header "X-Api-Token: $(Token)" -F "metadata={changelog: \"\", releaseType: \"alpha\", gameVersions: [ 6756 ] }" -F file=@anyseed-1.12.2-1.1.1-beta-universal.jar https://minecraft.curseforge.com/api/projects/AtomicBlomsTestCurseForgeAPIProject/upload-file
+        //curl -v --header "X-Api-Token: $(Token)" -F "metadata={changelog: \"\", releaseType: \"alpha\", gameVersions: [ 6756 ] }" -F file=@anyseed-1.12.2-1.1.1-beta-universal.jar https://minecraft.curseforge.com/api/projects/304919/upload-file
+        //curl -v --header "Expect:" --header "X-Api-Token: $(Token)" -F "metadata={changelog: \"\", releaseType: \"alpha\", gameVersions: [ 6756 ] }" -F file=@anyseed-1.12.2-1.0-universal.jar https://minecraft.curseforge.com/api/projects/304919/upload-file
+
         tl.debug('CurseForgeService:' + serviceName)
     } catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
