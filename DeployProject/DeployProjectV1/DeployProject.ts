4e75f5ba837edcd0e669e3873f592f1ce1bf401a
Steven Blom <atomicblom@binaryvibrance.net>
2018-10-17 21:34:17 +1030

    Reset everything now that I know what I'm doing

--- DeployProject/DeployProjectV1/DeployProject.ts
+++ DeployProject/DeployProjectV1/DeployProject.ts
@@ -1,18 +1,33 @@
-import tl = require('azure-pipelines-task-lib/task');
-//npm install vsts-task-lib
+import * as tl from 'azure-pipelines-task-lib/task';
+import * as unirest from 'unirest';
+import { IProjectUpload } from './CurseForgeAPI/IProjectUpload';
 
-// Get task parameters
-let variable1: string = tl.getPathInput('variable1', false, true);
-let variable2: string = tl.getInput('variable2', true);
-
-
+let serviceName: string = tl.getInput('CurseForgeService', true);
+let file = tl.getPathInput("ProjectFile", true, true);
+let projectId = tl.getInput('Project', true);
+let releaseType = <"alpha" | "beta" | "release">tl.getInput('ReleaseType', true);
 
 async function run() {
     try {
-        //do your actions
-        tl.debug('variable1:' +variable1)
-        tl.debug('variable2:' +variable2)
-        
+        const authorization = tl.getEndpointAuthorization(serviceName, false);
+        if (authorization.scheme !== "Token") {
+            throw Error("TODO: error this properly");
+        }
+
+        const metadata: IProjectUpload = {
+            changelog: "",
+            releaseType: releaseType,
+            gameVersions: [ 6756 ],
+        };
+
+        unirest.post(`https://minecraft.curseforge.com/api/projects/${projectId}/upload-file`)
+            .headers({
+                'Content-Type': 'multipart/form-data',
+                'X-Api-Token': authorization.parameters.apitoken
+            })
+            .field('metadata', JSON.stringify(metadata))
+            .attach('file', file)
+            .end( (response) => console.log(response.body) );
     } catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
