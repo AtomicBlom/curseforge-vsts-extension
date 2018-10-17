5e0a0d716e7ab3c6c5e2ad5f789ec34c954d6eec
Steven Blom <atomicblom@binaryvibrance.net>
2018-10-17 21:15:16 +1030

    First actual working version

--- DeployProject/DeployProjectV5/DeployProject.ts
+++ DeployProject/DeployProjectV5/DeployProject.ts
@@ -1,40 +1,33 @@
-import tl = require('azure-pipelines-task-lib/task');
-import fs = require('fs');
-import { ConnectedService } from './CurseForgeAPI/ConnectedService';
-import { WebResource } from 'ms-rest-js';
+import * as tl from 'azure-pipelines-task-lib/task';
+import * as unirest from 'unirest';
 import { IProjectUpload } from './CurseForgeAPI/IProjectUpload';
 
-//npm install vsts-task-lib
-
-// Get task parameters
 let serviceName: string = tl.getInput('CurseForgeService', true);
 let file = tl.getPathInput("ProjectFile", true, true);
+let projectId = tl.getInput('Project', true);
+let releaseType = <"alpha" | "beta" | "release">tl.getInput('ReleaseType', true);
+
 async function run() {
     try {
-
-        /*const service = new ConnectedService(serviceName);
-        const webResource = new WebResource();
-        
-        const requestForm = new FormData();
-        const metadata: IProjectUpload = {
-            changelog: "",
-            releaseType: "alpha",
-            gameVersions: [ 6756 ],
+        const authorization = tl.getEndpointAuthorization(serviceName, false);
+        if (authorization.scheme !== "Token") {
+            throw Error("TODO: error this properly");
         }
 
-        const reader = new FileReader();
-        reader.
-        requestForm.append("metadata", JSON.stringify(metadata));
-        requestForm.append("file", new File(); )
+        const metadata: IProjectUpload = {
+            changelog: "",
+            releaseType: releaseType,
+            gameVersions: [ 6756 ],
+        };
 
-        service.sendRequest()*/
-
-        //Almost working CURL request?:
-        //curl -X POST --header "X-Api-Token: $(Token)" -F "metadata={changelog: \"\", releaseType: \"alpha\", gameVersions: [ 6756 ] }" -F file=@anyseed-1.12.2-1.1.1-beta-universal.jar https://minecraft.curseforge.com/api/projects/AtomicBlomsTestCurseForgeAPIProject/upload-file
-        //curl -v --header "X-Api-Token: $(Token)" -F "metadata={changelog: \"\", releaseType: \"alpha\", gameVersions: [ 6756 ] }" -F file=@anyseed-1.12.2-1.1.1-beta-universal.jar https://minecraft.curseforge.com/api/projects/304919/upload-file
-        //curl -v --header "Expect:" --header "X-Api-Token: $(Token)" -F "metadata={changelog: \"\", releaseType: \"alpha\", gameVersions: [ 6756 ] }" -F file=@anyseed-1.12.2-1.0-universal.jar https://minecraft.curseforge.com/api/projects/304919/upload-file
-
-        tl.debug('CurseForgeService:' + serviceName)
+        unirest.post(`https://minecraft.curseforge.com/api/projects/${projectId}/upload-file`)
+            .headers({
+                'Content-Type': 'multipart/form-data',
+                'X-Api-Token': authorization.parameters.apitoken
+            })
+            .field('metadata', JSON.stringify(metadata))
+            .attach('file', file)
+            .end( (response) => console.log(response.body) );
     } catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
